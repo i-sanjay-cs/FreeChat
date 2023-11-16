0582fe0880fc2b3fa3150ada5e0f368ef95876fc
i-sanjay-cs <sanjay826870@gmail.com>
2023-11-16 11:49:29 +0530

    changes

--- main.py
+++ main.py
@@ -1,25 +1,9 @@
-# app.py
 from flask import Flask, render_template, request, redirect, url_for
 from flask_socketio import SocketIO, join_room, emit
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime, timedelta
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'  # Use SQLite for simplicity
-db = SQLAlchemy(app)
 socketio = SocketIO(app)
 rooms = {}  # Dictionary to store room information
-
-import sqlite3
-
-conn = sqlite3.connect('site.db')
-cursor = conn.cursor()
-
-# Now you can execute SQL queries, e.g., cursor.execute("SELECT * FROM chat_room;")
-
-class ChatRoom(db.Model):
-    id = db.Column(db.String(10), primary_key=True)
-    expiration_time = db.Column(db.DateTime, nullable=False)
 
 @app.route('/')
 def index():
@@ -30,13 +14,7 @@ def main():
     if request.method == 'POST':
         name = request.form['name']
         code = request.form['code']
-
-        # Check if the room code exists and is not expired
-        room = ChatRoom.query.filter_by(id=code).first()
-        if room and room.expiration_time > datetime.utcnow():
-            return redirect(url_for('chat', name=name, code=code))
-        else:
-            return render_template('index.html', error='Invalid or expired code. Please try again.')
+        return redirect(url_for('chat', name=name, code=code))
 
     return render_template('index.html')
 
@@ -51,28 +29,10 @@ def handle_join(data):
 
     print(f"Join request received: {name}, {code}")
 
-    # Check if the room code exists and is not expired
-    room = ChatRoom.query.filter_by(id=code).first()
-    if room and room.expiration_time > datetime.utcnow():
-        join_room(code)  # Use join_room to add the user to the room
-        rooms[request.sid] = code  # Update rooms dictionary with user's room
-        print("User joined successfully.")
-        emit('chat', {'message': f'{name} has joined the chat', 'name': 'System'}, room=code)
-    else:
-        # Create a new ChatRoom record with a future expiration time
-        expiration_time = datetime.utcnow() + timedelta(days=1)
-        new_room = ChatRoom(id=code, expiration_time=expiration_time)
-        db.session.add(new_room)
-        db.session.commit()
-
-        # Inform the user about the successful join
-        join_room(code)
-        rooms[request.sid] = code
-        print("User joined successfully.")
-        emit('chat', {'message': f'{name} has joined the chat', 'name': 'System'}, room=code)
-
-
-
+    join_room(code)  # Use join_room to add the user to the room
+    rooms[request.sid] = code  # Update rooms dictionary with user's room
+    print("User joined successfully.")
+    emit('chat', {'message': f'{name} has joined the chat', 'name': 'System'}, room=code)
 
 @socketio.on('message')
 def handle_message(data):
@@ -86,7 +46,6 @@ def handle_message(data):
     else:
         print(f"User {name} is not in a room. Ignoring message.")
 
-
 @socketio.on('disconnect')
 def handle_disconnect():
     sid = request.sid
@@ -96,6 +55,4 @@ def handle_disconnect():
         emit('chat', {'message': 'A user has left the chat', 'name': 'System'}, room=code)
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     socketio.run(app, debug=True)
